d4d788185df104ab651d50064420ebf7cbb54dcc
salome.burteaux <salome.burteaux@le-campus-numerique.fr>
2025-11-17 16:07:13 +0100

    amélioration code message erreur server (main)

--- Server/src/main/java/org/example/Main.java
+++ Server/src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import org.apache.commons.lang3.StringUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +8,10 @@ public class Main {
         int p = 12345;
         Server s = new Server(p);
         try {
+            System.out.println("Starting server...");
             s.start();
         } catch (IOException e) {
-            System.out.println("erreur");
+            System.err.println("Server failed: " + e.getMessage());
         }
-        // System.out.println("Code inutile");
-        // ArrayList<String> temp = new ArrayList<>();
     }
 }
